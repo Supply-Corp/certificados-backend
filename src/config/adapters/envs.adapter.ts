9dd029c0c9831da41a684aa7aae326bea459a4ae
zandrij <ajosuesh@gmail.com>
2024-01-16 09:37:43 -0500

    refactor: metodo de conexion a la base de datos

--- src/config/adapters/envs.adapter.ts
+++ src/config/adapters/envs.adapter.ts
@@ -8,7 +8,7 @@ export const envs = {
     PORT: get('PORT').required().asPortNumber(),
     PUBLIC_PATH: get('PUBLIC_PATH').required().asString(),
 
-    DATABASE_URL: get('DATABASE_URL').required().asString(),
+    // DATABASE_URL: get('DATABASE_URL').required().asString(),
     JWT_SEED: get('JWT_SEED').required().asString(),
 
     MAILER_SERVICE: get('MAILER_SERVICE').required().asString(),
@@ -16,4 +16,11 @@ export const envs = {
     MAILER_SECRET: get('MAILER_SECRET').required().asString(),
 
     WEB_SERVICE_URL: get('WEB_SERVICE_URL').required().asString(),
+
+    DATABASE_HOST: get('DATABASE_HOST').required().asString(),
+    DATABASE_USER: get('DATABASE_USER').required().asString(),
+    DATABASE_PASSWORD: get('DATABASE_PASSWORD').required().asString(),
+    DATABASE_PORT: get('DATABASE_PORT').required().asPortNumber(),
+    DATABASE_DIALECT: get('DATABASE_DIALECT').required().asString(),
+    DATABASE_NAME: get('DATABASE_NAME').required().asString(),
 }
--- src/data/mysql/sequelize/index.ts
+++ src/data/mysql/sequelize/index.ts
@@ -1,4 +1,11 @@
 import { Sequelize } from "sequelize";
 import { envs } from "../../../config";
 
-export const sequelize = new Sequelize(`${envs.DATABASE_URL}`);
+export const sequelize = new Sequelize({
+    host: envs.DATABASE_HOST,
+    username: envs.DATABASE_USER,
+    password: envs.DATABASE_PASSWORD,
+    database: envs.DATABASE_NAME,
+    port: envs.DATABASE_PORT,
+    dialect: 'mysql'
+});
