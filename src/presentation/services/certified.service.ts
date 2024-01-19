1e8faa4cb79fba8715a8e6be9739d8efea7b01fe
zandrij <ajosuesh@gmail.com>
2024-01-19 12:05:13 -0500

    test

--- src/presentation/services/certified.service.ts
+++ src/presentation/services/certified.service.ts
@@ -54,6 +54,7 @@ export class CertifiedService {
                         font-size: 70px;
                         margin-top: 0;
                         color: #a47517;
+                        font-weight: 300;
                     }
                     .p-one {
                         display: block;
