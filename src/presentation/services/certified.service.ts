7de9da09a9c359d45335de08f0564b58f32e134d
zandrij <ajosuesh@gmail.com>
2024-01-19 14:44:31 -0500

    test: font-weight

--- src/presentation/services/certified.service.ts
+++ src/presentation/services/certified.service.ts
@@ -86,7 +86,7 @@ export class CertifiedService {
                         margin-top: 0px;
                         margin: auto;
                         height: fit-content;
-                        font-weight: 300;
+                        font-weight: 400;
                     }
                     .p-tree {
                         display: block;
