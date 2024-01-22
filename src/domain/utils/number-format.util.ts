31c76d914e3052d5711df3992cdc72654ce2057c
zandrij <ajosuesh@gmail.com>
2024-01-22 11:43:41 -0500

    fix: number to string

--- src/domain/utils/number-format.util.ts
+++ src/domain/utils/number-format.util.ts
@@ -1,7 +1,7 @@
 export function numeroALetras(num: number): string {
     const unidades = ['', 'uno', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve'];
     const decenas = ['', 'diez', 'veinte', 'treinta', 'cuarenta', 'cincuenta', 'sesenta', 'setenta', 'ochenta', 'noventa'];
-    const centenas = ['', 'cien', 'doscientos', 'trescientos', 'cuatrocientos', 'quinientos', 'seiscientos', 'setecientos', 'ochocientos', 'novecientos'];
+    const centenas = ['', 'ciento', 'doscientos', 'trescientos', 'cuatrocientos', 'quinientos', 'seiscientos', 'setecientos', 'ochocientos', 'novecientos'];
     const especiales: { [key: string]: string } = { '11': 'once', '12': 'doce', '13': 'trece', '14': 'catorce', '15': 'quince' };
 
     if (num <= 10) {
@@ -11,7 +11,11 @@ export function numeroALetras(num: number): string {
     } else if (num < 100) {
         return decenas[Math.floor(num / 10)] + (num % 10 !== 0 ? ' y ' + unidades[num % 10] : '');
     } else if (num < 1000) {
-        return centenas[Math.floor(num / 100)] + (num % 100 !== 0 ? ' ' + numeroALetras(num % 100) : '');
+        if (num === 100) {
+            return 'cien';
+        } else {
+            return centenas[Math.floor(num / 100)] + (num % 100 !== 0 ? ' ' + numeroALetras(num % 100) : '');
+        }
     } else if (num <= 9999) {
         let mil = Math.floor(num / 1000);
         let resto = num % 1000;
